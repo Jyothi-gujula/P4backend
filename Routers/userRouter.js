83f68177517567364b7cedf5e08a3260fa028ded
Jyothi-gujula <22r01a7328@cmrithyderabad.edu.in>
2025-03-02 13:48:08 +0530

    Add files via upload

---
+++ Routers/userRouter.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { loginControllers, registerControllers, setAvatarController, allUsers } from '../controllers/userController.js';
+import { authenticate } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route("/register").post(registerControllers);
+
+router.route("/login").post(loginControllers);
+
+router.route("/setAvatar/:id").post(authenticate, setAvatarController);
+
+router.route("/profile").get(authenticate, allUsers);
+
+
+export default router;
